75e942935205642c0763d690108ed308f588485c
Bora DAN <bora_dan@hotmail.com>
2021-11-22 15:17:10 +0300

    turkey-demo file added

--- turkey-map/js/main.js
+++ turkey-map/js/main.js
@@ -1,7 +1,7 @@
 
   function GetMap() {
     var city = $('#turkey-map').data('city') == undefined ? 'turkey' : $('#turkey-map').data('city');
-    $.get('turkey-map/source/' + city + '.svg', function(data) {
+    $.get('turkey-map/source/turkey-demo.svg', function(data) {
       $('#turkey-map').append('<div class="map-container">' + data + '</div>');
       $('.map-container').append('<div class="map-title"><span class="map-close"></span><strong></strong></div>');
 
